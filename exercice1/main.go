11b7d10500d0516e73f367a5a0e451e59a36e2f3
mistergaming111-netizen <mistergaming111@gmail.com>
2026-09-11 16:12:07 +0200

    Ajout affichage boisson

--- exercice1/main.go
+++ exercice1/main.go
@@ -3,7 +3,34 @@ package main
 import ("fmt")
 	
 func main() {
+	afficherMenu()
+	var choix int
+	fmt.Print("Veuillez entrer votre choix : ")
+	fmt.Scanln(&choix)
+	if choix == 0 {
+		fmt.Println("Merci d'avoir utilisé le distributeur. Au revoir !")
+		return
+	} else if choix < 1 || choix > 4 {
+		fmt.Println("Choix invalide. Veuillez réessayer.")
+		return
+	} else {
+	afficherBoisson(choix)
+	prix := obtenirPrix(choix)
+	fmt.Printf("Le prix de votre boisson est de %d €\n", prix)
+	fmt.Print("Veuillez entrer le montant que vous insérez : ")
+	var montant int
+	fmt.Scanln(&montant)
+	for montant < prix {
 
+		for montant < prix {
+    fmt.Printf("Montant insuffisant. Il vous manque %d €.\n", prix-montant)
+    fmt.Print("Veuillez entrer le montant que vous insérez : ")
+    var ajout int
+    fmt.Scanln(&ajout)
+    montant += ajout
+}
+		}
+	}
 }
 
 func afficherMenu() {
@@ -16,3 +43,31 @@ func afficherMenu() {
 	fmt.Println("0 - Quitter")
 	fmt.Println()
 }
+
+func obtenirPrix(choix int) int {
+	if choix == 1 {
+		return 1
+	} else if choix == 2 {
+		return 2
+	} else if choix == 3 {
+		return 2
+	} else if choix == 4 {
+		return 3
+	} else {
+		return 0 
+	}
+}
+	
+func afficherBoisson(choix int) {
+	if choix == 1 {
+		fmt.Println("Vous avez choisi de l'eau.")
+	} else if choix == 2 {
+		fmt.Println("Vous avez choisi un soda.")
+	} else if choix == 3 {
+		fmt.Println("Vous avez choisi du café.")
+	} else if choix == 4 {
+		fmt.Println("Vous avez choisi du chocolat.") 
+	}
+}
+
+
