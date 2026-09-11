a1ccc426731f6c95a8295db75977493cd646b00d
mistergaming111-netizen <mistergaming111@gmail.com>
2026-09-11 14:56:39 +0200

    Ajout affichage boisson

---
+++ exercice1/main.go
@@ -0,0 +1,18 @@
+package main
+
+import ("fmt")
+	
+func main() {
+
+}
+
+func afficherMenu() {
+	fmt.Println("=== DISTRIBUTEUR ===")
+
+	fmt.Println("1 - Eau       : 1 €")
+	fmt.Println("2 - Soda      : 2 €")
+	fmt.Println("3 - Café      : 2 €")
+	fmt.Println("4 - Chocolat  : 3 €")
+	fmt.Println("0 - Quitter")
+	fmt.Println()
+}
